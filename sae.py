2a76cb79f267f05cdcd253fc38b3f66cccaf5108
matospiso <masp430@gmail.com>
2025-01-12 23:42:56 +0100

    initial sae, train_sae

---
+++ sae.py
@@ -0,0 +1,73 @@
+from abc import abstractmethod
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from util import l2_normalize
+
+
+class SAE(nn.Module):
+    def __init__(self, input_dim: int, embedding_dim: int, seed: int):
+        super().__init__()
+        rng = torch.Generator()
+        rng.manual_seed(seed)
+        self.encoder_w = nn.Parameter(nn.init.kaiming_uniform_(torch.empty([input_dim, embedding_dim]), generator=rng))
+        self.encoder_b = nn.Parameter(torch.zeros(embedding_dim))
+        self.decoder_w = nn.Parameter(nn.init.kaiming_uniform_(torch.empty([embedding_dim, input_dim]), generator=rng))
+        self.decoder_b = nn.Parameter(torch.zeros(input_dim))
+        self.normalize_decoder()
+
+    @abstractmethod
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, dict]:
+        raise NotImplementedError
+
+    @torch.no_grad()
+    def normalize_decoder(self) -> None:
+        self.decoder_w.data = l2_normalize(self.decoder_w.data)
+        if self.decoder_w.grad is not None:
+            self.decoder_w.grad -= (self.decoder_w.grad * self.decoder_w.data).sum(-1, keepdim=True) * self.decoder_w.data
+
+    def standardize_input(self, x: torch.Tensor) -> torch.Tensor:
+        x_mean = x.mean(dim=-1, keepdim=True)
+        x -= x_mean
+        x_std = x.std(dim=-1, keepdim=True)
+        x /= x_std + 1e-7
+        return x, x_mean, x_std
+
+    def destandardize_output(self, x: torch.Tensor, x_mean: torch.Tensor, x_std: torch.Tensor) -> torch.Tensor:
+        return x_mean + x * x_std
+
+    def train_step(self, optimizer: optim.Optimizer, batch: torch.Tensor) -> float:
+        _, losses = self(batch)
+        optimizer.zero_grad()
+        losses["Loss"].backward()
+        self.normalize_decoder()
+        optimizer.step()
+        return {k: v.item() for k, v in losses.items()}
+
+
+class BasicSAE(SAE):
+    def __init__(self, input_dim: int, embedding_dim: int, seed: int):
+        super().__init__(input_dim, embedding_dim, seed)
+
+    def compute_losses(self, x: torch.Tensor, e: torch.Tensor, x_out: torch.Tensor) -> dict:
+        l2_loss = (x_out - x).pow(2).sum(-1).sqrt().div(x.norm(dim=-1)).mean()
+        l1_loss = e.abs().sum(-1).mean()
+        l0_loss = (e > 0).float().sum(-1).mean()
+        loss = l2_loss + 0.01 * l1_loss
+        return {"Loss": loss, "L2": l2_loss, "L1": l1_loss, "L0": l0_loss}
+
+    def forward(self, x: torch.Tensor) -> tuple[torch.Tensor, dict]:
+        x, x_mean, x_std = self.standardize_input(x)
+        e = F.relu((x - self.decoder_b) @ self.encoder_w + self.encoder_b)
+        x_out = e @ self.decoder_w + self.decoder_b
+        if not self.training:
+            return self.destandardize_output(x_out, x_mean, x_std)
+        return self.destandardize_output(x_out, x_mean, x_std), self.compute_losses(x, e, x_out)
+
+
+class TopKSAE(SAE): ...
+
+
+class BatchTopKSAE(SAE): ...
---
+++ train_sae.py
@@ -0,0 +1,102 @@
+import argparse
+import importlib
+import numpy as np
+import torch
+import torch.optim as optim
+from tqdm import tqdm
+import scipy.sparse as sp
+
+from datasets import Dataloader, convert_to_csr, load_interactions, split_input_target_interactions, split_train_val_test_users
+from util import CHECKPOINT_FOLDER, get_checkpoint_filepath, load_config_from_checkpoint, save_checkpoint, load_checkpoint
+
+
+def evaluate_recall_at_k(model, input_csr: sp.csr_matrix, target_csr: sp.csr_matrix, k: int, batch_size: int, device: torch.device) -> np.ndarray:
+    sample_size = input_csr.shape[0]
+    batch_count = -(-sample_size // batch_size)
+    metric_array = []
+    for i in range(batch_count):
+        input_batch = torch.tensor(input_csr[i * batch_size : min(sample_size, (i + 1) * batch_size)].toarray(), device=device)
+        target_batch = target_csr[i * batch_size : min(sample_size, (i + 1) * batch_size)]
+        scores = model(input_batch)
+        scores = torch.where(input_batch != 0, 0, scores)
+        topk_scores, topk_indices = torch.topk(scores, k)
+        topk_indices = topk_indices.detach().cpu().numpy()
+        batch_results = []
+        for i in range(target_batch.shape[0]):
+            predicted_indices = topk_indices[i]
+            target_indices = target_batch.indices[target_batch.indptr[i] : target_batch.indptr[i + 1]]
+            r = np.isin(target_indices, predicted_indices, assume_unique=True).sum() / len(target_indices)
+            batch_results.append(r)
+        metric_array.extend(batch_results)
+    return np.array(metric_array)
+
+
+def train_sae(cfg: dict, device: torch.device):
+    print(f"Training sparse autoencoder using config {cfg}")
+
+    pretrained_model_checkpoint = f"{CHECKPOINT_FOLDER}/{cfg['dataset']}/{cfg['pretrained_model_checkpoint']}"
+    pretrained_model_cfg = load_config_from_checkpoint(pretrained_model_checkpoint)
+    print(f"Source model config: {pretrained_model_cfg}")
+
+    dataset = cfg["dataset"]
+    interactions_df = load_interactions(dataset)
+    interactions_csr = convert_to_csr(interactions_df)
+    train_csr, val_csr, _ = split_train_val_test_users(
+        interactions_csr, pretrained_model_cfg["val_user_ratio"], pretrained_model_cfg["test_user_ratio"], pretrained_model_cfg["seed"]
+    )
+    interaction_dataloader = Dataloader(train_csr, pretrained_model_cfg["batch_size"], device, pretrained_model_cfg["seed"])
+
+    pretrained_model_class = getattr(importlib.import_module(pretrained_model_cfg["model_module"]), pretrained_model_cfg["model_class"])
+    pretrained_model = pretrained_model_class(train_csr.shape[1], pretrained_model_cfg["embedding_dim"], pretrained_model_cfg["seed"]).to(device)
+    load_checkpoint(pretrained_model, None, pretrained_model_checkpoint, device)
+    user_embeddings = np.vstack(
+        [pretrained_model.encode(batch).detach().cpu().numpy() for batch in tqdm(interaction_dataloader, desc="Computing user embeddings from interactions")]
+    )
+    print(f"User embeddings shape={user_embeddings.shape}")
+
+    dataloader = Dataloader(user_embeddings, cfg["batch_size"], device, cfg["seed"])
+    model_class = getattr(importlib.import_module(cfg["model_module"]), cfg["model_class"])
+    model = model_class(user_embeddings.shape[1], cfg["embedding_dim"], cfg["seed"]).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=cfg["lr"])
+
+    checkpoint_path = get_checkpoint_filepath(cfg)
+    try:
+        start_epoch, _ = load_checkpoint(model, optimizer, checkpoint_path, device, cfg)
+    except FileNotFoundError:
+        print("No checkpoint found, starting from scratch.")
+        start_epoch = 0
+
+    best_result = 0.0
+    for epoch in range(start_epoch, cfg["epochs"]):
+        model.train()
+        pbar = tqdm(dataloader)
+        for i, batch in enumerate(pbar):
+            losses = model.train_step(optimizer, batch)
+            pbar.set_description(f"Epoch {epoch + 1}/{cfg['epochs']}", refresh=False)
+            pbar.set_postfix(losses)
+            # if i == len(pbar) - 1:
+            #     model.eval()
+            #     val_inputs, val_targets = split_input_target_interactions(val_csr, cfg["target_interaction_ratio"], cfg["seed"])
+            #     eval_results = evaluate_recall_at_k(model, val_inputs, val_targets, cfg["eval_topk"], cfg["batch_size"], device)
+            #     pbar.set_postfix_str(
+            #         pbar.postfix + f", Recall@{cfg['eval_topk']}={np.mean(eval_results):.4f}+-{np.std(eval_results) / np.sqrt(len(eval_results)):.4f}"
+            #     )
+        # if best_result < np.mean(eval_results):
+        #     best_result = np.mean(eval_results)
+        #     save_checkpoint(model, optimizer, epoch + 1, cfg, checkpoint_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Argument parser for SAE training script.")
+    parser.add_argument("--dataset", type=str, required=True, help="Dataset name")
+    parser.add_argument("--pretrained_model_checkpoint", type=str, required=True, help="Filename of checkpoint containing pre-trained model")
+    parser.add_argument("--model_module", type=str, default="sae", help="Module containing SAE model")
+    parser.add_argument("--model_class", type=str, default="BasicSAE", help="Model class name")
+    parser.add_argument("--embedding_dim", type=int, required=True, help="Embedding dimension of SAE model")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("--batch_size", type=int, default=1024, help="Batch size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--seed", type=float, default=42, help="Random seed")
+    cfg = vars(parser.parse_args())
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.mps.is_available() else torch.device("cpu")
+    train_sae(cfg, device)
