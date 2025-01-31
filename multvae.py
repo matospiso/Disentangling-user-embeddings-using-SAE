a86d9ee112fa6b179a742636ad0d0bc337e000e1
matospiso <masp430@gmail.com>
2025-01-31 00:34:50 +0000

    multvae (2 TODOs to make it final)

---
+++ multvae.py
@@ -0,0 +1,81 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+
+class Linear(nn.Module):
+    def __init__(self, weight: torch.Tensor, bias: torch.Tensor):
+        super().__init__()
+        self.weight = nn.Parameter(weight)
+        self.bias = nn.Parameter(bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.linear(x, self.weight, self.bias)
+
+
+class MultVAE(nn.Module):
+    """Multinomial Variational Autoencoder for collaborative filtering
+    Paper: https://arxiv.org/pdf/1802.05814"""
+
+    def __init__(self, input_dim: int, hidden_dims: list[int], embedding_dim: int, seed: int):
+        super().__init__()
+        self.rng = torch.Generator()
+        self.rng.manual_seed(seed)
+
+        dims = [input_dim] + hidden_dims + [embedding_dim]
+        self.encoder = nn.Sequential()
+        for i in range(len(dims) - 2):
+            linear = Linear(nn.init.xavier_uniform_(torch.empty([dims[i + 1], dims[i]]), generator=self.rng), torch.zeros(dims[i + 1]))
+            self.encoder.add_module("fc{}".format(i), linear)
+            self.encoder.add_module("act{}".format(i), nn.Tanh())
+        self.encoder_mu = Linear(nn.init.xavier_uniform_(torch.empty([dims[-1], dims[-2]]), generator=self.rng), torch.zeros(dims[-1]))
+        self.encoder_logvar = Linear(nn.init.xavier_uniform_(torch.empty([dims[-1], dims[-2]]), generator=self.rng), torch.zeros(dims[-1]))
+
+        dims = dims[::-1]
+        self.decoder = nn.Sequential()
+        for i in range(len(dims) - 1):
+            w = nn.init.xavier_uniform_(torch.empty([dims[i + 1], dims[i]]), generator=self.rng)
+            b = torch.zeros(dims[i + 1])
+            self.decoder.add_module("fc{}".format(i), Linear(w, b))
+            if i != len(dims) - 2:
+                self.decoder.add_module("act{}".format(i), nn.Tanh())
+
+    def encode(self, x: torch.Tensor) -> torch.Tensor:
+        h = self.encoder(x)
+        return self.encoder_mu(h), self.encoder_logvar(h)
+
+    def sample_from_prior(self, mu: torch.Tensor, logvar: torch.Tensor) -> torch.Tensor:
+        eps = torch.randn(mu.shape, dtype=mu.dtype, generator=self.rng).to(mu.device)
+        return mu + eps * logvar.exp().sqrt()
+
+    def decode(self, e: torch.Tensor) -> torch.Tensor:
+        return torch.softmax(self.decoder(e), dim=-1)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        mu, logvar = self.encode(x)
+        if self.training:
+            return self.decode(self.sample_from_prior(mu, logvar))
+        return self.decode(mu)
+
+    def compute_loss_dict(self, batch: torch.Tensor) -> dict[str, torch.Tensor]:
+        mu, logvar = self.encode(nn.functional.dropout(batch, 0.5))  # TODO refactor away + fix seed
+        out = self.decode(self.sample_from_prior(mu, logvar))
+        beta = 0.2  # TODO implement annealing
+        nll = -torch.sum(batch * torch.log(torch.clamp(out, min=1e-7)), dim=-1)
+        d_kl = torch.sum(-0.5 * (1 + logvar - mu.pow(2) - torch.exp(logvar)), dim=-1)
+        return {"NLL": torch.mean(nll), "D_KL": torch.mean(d_kl), "Loss": torch.mean(nll + beta * d_kl)}
+
+    def train_step(self, optimizer: optim.Optimizer, batch: torch.Tensor) -> dict[str, torch.Tensor]:
+        losses = self.compute_loss_dict(batch)
+        optimizer.zero_grad()
+        losses["Loss"].backward()
+        optimizer.step()
+        return losses
+
+    @torch.no_grad()
+    def recommend(self, interaction_batch: torch.Tensor, k: int, mask_interactions: bool = True) -> tuple[torch.Tensor, torch.Tensor]:
+        scores = self(interaction_batch)
+        if mask_interactions:
+            scores = torch.where(interaction_batch != 0, 0, scores)  # mask input interactions
+        topk_scores, topk_indices = torch.topk(scores, k)
+        return topk_scores.cpu().numpy(), topk_indices.cpu().numpy()
---
+++ train_multvae.py
@@ -0,0 +1,55 @@
+import argparse
+import importlib
+import numpy as np
+import torch
+import torch.optim as optim
+
+from datasets import Dataloader, prepare_interaction_data, split_input_target_interactions
+from util import evaluate_recall_at_k, get_checkpoint_filepath, get_checkpoint_name, load_checkpoint, run_training_loop
+
+
+def train_multvae(cfg: dict, device: torch.device):
+    print(f"Training MultVAE model using config {cfg}")
+
+    _, train_csr, val_csr, _, _, _, _, _ = prepare_interaction_data(cfg)
+    train_dataloader = Dataloader(train_csr, cfg["batch_size"], device, cfg["seed"])
+    val_dataloader = Dataloader(val_csr, cfg["batch_size"], device)
+
+    model_class = getattr(importlib.import_module(cfg["model_module"]), cfg["model_class"])
+    model = model_class(train_csr.shape[1], [int(x) for x in cfg["hidden_dims"].split(",")], cfg["embedding_dim"], cfg["seed"]).to(device)
+    optimizer = optim.Adam(model.parameters(), lr=cfg["lr"], betas=(cfg["beta1"], cfg["beta2"]))
+
+    run_training_loop(model, optimizer, train_dataloader, val_dataloader, cfg, device)
+
+    load_checkpoint(model, None, get_checkpoint_filepath(cfg), device, cfg)
+    model.eval()
+    val_inputs, val_targets = split_input_target_interactions(val_csr, cfg["target_interaction_ratio"], cfg["seed"])
+    eval_results = evaluate_recall_at_k(
+        model, Dataloader(val_inputs, cfg["batch_size"], device), Dataloader(val_targets, cfg["batch_size"], device), cfg["eval_topk"]
+    )
+    print(
+        f"Model = {get_checkpoint_name(cfg)} | Recall @ {cfg['eval_topk']} = {np.mean(eval_results):.6f} +- {np.std(eval_results) / np.sqrt(len(eval_results)):.6f}"
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Argument parser for MultVAE training script.")
+    parser.add_argument("--dataset", type=str, required=True, help="Dataset name")
+    parser.add_argument("--val_user_ratio", type=float, default=0.1, help="Ratio of validation users")
+    parser.add_argument("--test_user_ratio", type=float, default=0.1, help="Ratio of test users")
+    parser.add_argument("--target_interaction_ratio", type=float, default=0.2, help="Ratio of interactions used as target")
+    parser.add_argument("--model_module", type=str, default="multvae", help="Module containing MultVAE model")
+    parser.add_argument("--model_class", type=str, default="MultVAE", help="Model class name")
+    parser.add_argument("--hidden_dims", type=str, default="", help="Comma-separated hidden layer dimensions")
+    parser.add_argument("--embedding_dim", type=int, required=True, help="Embedding dimension of MultVAE model")
+    parser.add_argument("--epochs", type=int, default=10, help="Number of epochs")
+    parser.add_argument("--early_stopping", type=int, default=10, help="Early stopping number of epochs")
+    parser.add_argument("--batch_size", type=int, default=1024, help="Batch size")
+    parser.add_argument("--lr", type=float, default=1e-4, help="Learning rate")
+    parser.add_argument("--beta1", type=float, default=0.9, help="Adam beta_1 coefficient")
+    parser.add_argument("--beta2", type=float, default=0.99, help="Adam beta_2 coefficient")
+    parser.add_argument("--eval_topk", type=int, default=20, help="Evalutation top k")
+    parser.add_argument("--seed", type=float, default=42, help="Random seed")
+    cfg = vars(parser.parse_args())
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("mps") if torch.mps.is_available() else torch.device("cpu")
+    train_multvae(cfg, device)
